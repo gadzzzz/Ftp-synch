9a3a31db8287820d0bcd3ef16f9039e19476d548
gadzzzz <bogdanhadzhiev@yahoo.com>
2018-02-06 18:09:34 +0200

    test

--- src/main/java/com/synchftp/main/Application.java
+++ src/main/java/com/synchftp/main/Application.java
@@ -6,6 +6,8 @@ import com.synchftp.service.SynchUtil;
 import org.quartz.Scheduler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
@@ -18,7 +20,12 @@ import java.util.Properties;
  */
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.synchftp.controller","com.synchftp.service"})
-public class Application {
+public class Application  extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args){
         SpringApplication.run(Application.class,args);
